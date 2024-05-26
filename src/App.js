3275b7c3d42ed9e3705ea404b6092a07168458a1
M3 Beatz <m3beatz@M3.local>
2024-05-26 10:24:53 -0400

    Added fee info

--- src/App.js
+++ src/App.js
@@ -23,8 +23,10 @@ function App() {
       <section id="about" className="about-section">
         <h2>About $TON Mixer</h2>
         <p style={{ textAlign: 'center' }} className="description">$TON Mixer offers an easy solution for anonymizing your $TON transfers.</p>
-        <p style={{ textAlign: 'left' }} className="description"><ul><li>Simply input the amount of $TON you wish to mix, along with the destination address.</li><li>Then a deposit address is generated and displayed for you to send your $TON.</li> <li>Upon confirmation of deposit the bot will go through several steps to mix and finally return your $TON into the destination address.</li></ul> <em>During the process the mixer keeps a live update on the status of your exchange for you to monitor its progress.</em></p>
+        <p style={{ textAlign: 'left', paddingTop: 0, marginTop: 0 }} className="description"><ul><li>Simply input the amount of $TON you wish to mix, along with the destination address.</li><li>Then a deposit address is generated and displayed for you to send your $TON.</li> <li>Upon confirmation of deposit the bot will go through several steps to mix and finally return your $TON into the destination address.</li></ul> <em><br/>During the process the mixer keeps a live update on the status of your exchange for you to monitor its progress.</em></p>
         <p style={{ textAlign: 'center' }} className="description">This entire process allows users to scramble and mix their $TON such that one cannot follow the trail of transactions nor link your funds to any previous addresses.</p>
+        <p style={{ textAlign: 'center', paddingTop: 0, paddingBottom: 0, marginTop: '4px', marginBottom: 0 }} className="description">Fees: 0.8% of TX goes to revenue (buyback & burn)</p>
+        <p style={{ textAlign: 'center', paddingTop: 0, marginTop: 0 }} className="description">Network Fee: 0.003 - 3 TON (higher only if exchanging large amounts >= 80 TON)</p>
         <div className="screenshot-grid">
           <img src={screenshot0} alt="Screenshot 0" className="screenshot"/>
           <img src={screenshot1} alt="Screenshot 1" className="screenshot"/>
